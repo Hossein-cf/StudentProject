0dbab2915120047321a33c12b8a3fedcd47b9352
ghader-Salehi <amirghsl1380@gmail.com>
2020-04-15 12:46:05 -0700

    Classes created

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../FXMLs/LoginPage.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 837, 473));
         primaryStage.show();
     }
 
