104d90458d26465500d5d31b820babd8c324ac55
tic tac toe <shakryhsyn1@gmail.com>
2020-04-16 14:34:09 +0430

    add Student number and Master number and field serial producer class to extra package

---
+++ src/Extra/StudentAndMasterNumberAndFieldSerialProducer.java
@@ -0,0 +1,49 @@
+package Extra;
+
+public class StudentAndMasterNumberAndFieldSerialProducer extends Archive {
+
+    public long getStudentNumber() {
+
+        long number;
+        int numberOfStudent = getStudents().size();
+        String str = "98536";
+        number = Long.parseLong(getStringNumber(numberOfStudent, str));
+
+        return number;
+    }
+
+    public long getMasterNumber() {
+
+        long number;
+        int numberOfMaster = getMasters().size();
+        String str = "855494";
+        number = Long.parseLong(getStringNumber(numberOfMaster, str));
+
+        return number;
+    }
+
+    private String getStringNumber(int number, String str) {
+
+        if (number <= 9) {
+            str += "000" + number;
+        } else if (number <= 99) {
+            str += "00" + number;
+        } else if (number <= 999) {
+            str += "0" + number;
+        } else if (number <= 9999)
+            str += number;
+
+        return str;
+    }
+
+    public long getFieldSerial (){
+
+        long serial ;
+        int numberOfFields = getFields().size();
+        String str = "98";
+        serial = Long.parseLong(getStringNumber(numberOfFields,str));
+
+        return serial;
+    }
+
+}
